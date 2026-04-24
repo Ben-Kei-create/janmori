c11d090441e1c3617ba06835b9794c4418ea55d3
Ben-Kei-create <112257840+Ben-Kei-create@users.noreply.github.com>
2026-04-25 00:06:08 +0900

    Initial Commit

---
+++ JaKePa/JaKePaApp.swift
@@ -0,0 +1,17 @@
+//
+//  JaKePaApp.swift
+//  JaKePa
+//
+//  Created by 茂木史明 on 2026/04/25.
+//
+
+import SwiftUI
+
+@main
+struct JaKePaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
